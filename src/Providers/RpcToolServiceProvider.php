6ceda15cead99a5117db08057b40e35afd1ff11a
bulang <zhimeng0818@163.com>
2021-09-10 16:02:17 +0800

    ServiceProvider

---
+++ src/Providers/RpcToolServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Xiaohao\RpcTool\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+class RpcToolServiceProvider extends ServiceProvider {
+    public function boot() {
+
+    }
+
+    public function register () {
+
+    }
+}
